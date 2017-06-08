a92605859c3cc5dd37a866245931701a25815275
emhopkins <epoletto@lstreetc.com>
2017-06-07 22:31:58 -0500

    Added quote class and added comments explaining basic structure, ran bundler - generating gemfile.lock file

---
+++ lib/quote.rb
@@ -0,0 +1,10 @@
+class Quote
+	attr_accessor :characer, :content
+
+	# initialize will create quote objects with an optional name, content is required
+
+	#find all quotes for a given character
+
+	#list all quotes
+
+end
---
+++ lib/scraper.rb
@@ -0,0 +1,7 @@
+require 'open-uri'
+
+def scrape_office_quotes
+	# will scrape website for quotes and character names
+	# each quote will create a quote object
+
+end

1f86b66076993e0a098d3a3807981972dd573f53
emhopkins <epoletto@lstreetc.com>
2017-06-17 17:43:02 -0500

    adding basic functionality to quote class and creating new file for character class

--- lib/quote.rb
+++ lib/quote.rb
@@ -1,7 +1,25 @@
 class Quote
 	attr_accessor :characer, :content
 
-	# initialize will create quote objects with an optional name, content is required
+	@@all = []
+
+	def initialize(content, character="")
+		@content = content
+		if character != ""
+      		self.character=(character)
+    	end
+    end
+
+    def character=(character)
+    	@character = character
+    	@character.add_quote(self)
+ 	end
+
+	def self.all
+		@@all
+	end
+
+	# initialize will create quote objects with an optional character, content is required
 
 	#find all quotes for a given character
 
